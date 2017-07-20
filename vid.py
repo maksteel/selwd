8a93a595ca5f30aa095832f2c37b9da5b91534e1
maksteel <kukreja.m@gmail.com>
2017-07-20 00:22:59 +0000

    standing down for 5 mins in case of 'too many connection attempts' alert

--- vid.py
+++ vid.py
@@ -1,3 +1,9 @@
 from watch import *
-example = WatchYoutube(download_addon=False)
-example.thread.join()
+import time
+from selenium.common.exceptions import UnexpectedAlertPresentException
+try:
+        example = WatchYoutube(download_addon=False)
+        example.thread.join()
+except UnexpectedAlertPresentException:
+        print("Standing down")
+        time.sleep(300)
